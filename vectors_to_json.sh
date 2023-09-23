8be705dd0ef4fe57c1c34f6d1c5f65e41472fc58
Alfredo Garcia <oxarbitrage@gmail.com>
2023-09-23 19:40:43 -0300

    simplify jq command

--- vectors_to_json.sh
+++ vectors_to_json.sh
@@ -43,36 +43,22 @@ cat "$2" | grep -v -e "^====" -e "Test" -e "^$" | while read name; do
 	index3=$(echo "$stream3part1" | awk -F. '{print $1}' | awk -F'[' '{print $2}')
 	index4=$(echo "$stream4part1" | awk -F. '{print $1}' | awk -F'[' '{print $2}')
 
-	if [ $1 = 256 ]; then
-		jq -n \
-			--arg key1 $(echo $key1 | awk '{print $NF}') \
-			--arg key2 $(echo $key2) \
-			--arg iv $(echo $iv | awk '{print $NF}') \
-			--argjson stream1index $(echo $index1) \
-			--arg stream1expected $(echo $stream1part1 | awk '{print $NF}')$(echo $stream1part2)$(echo $stream1part3)$(echo $stream1part4) \
-			--argjson stream2index $(echo $index2) \
-			--arg stream2expected $(echo $stream2part1 | awk '{print $NF}')$(echo $stream2part2)$(echo $stream2part3)$(echo $stream2part4) \
-			--argjson stream3index $(echo $index3) \
-			--arg stream3expected $(echo $stream3part1 | awk '{print $NF}')$(echo $stream3part2)$(echo $stream3part3)$(echo $stream3part4) \
-			--argjson stream4index $(echo $index4) \
-			--arg stream4expected $(echo $stream4part1 | awk '{print $NF}')$(echo $stream4part2)$(echo $stream4part3)$(echo $stream4part4) \
-			--arg xordigest $(echo $xorpart1 | awk '{print $NF}')$(echo $xorpart2)$(echo $xorpart3)$(echo $xorpart4) \
-			'{'$name2':$ARGS.named}'
-	elif [ $1 = 128 ]; then
-		jq -n \
-			--arg key1 $(echo $key1 | awk '{print $NF}') \
-			--arg iv $(echo $iv | awk '{print $NF}') \
-			--argjson stream1index $(echo $index1) \
-			--arg stream1expected $(echo $stream1part1 | awk '{print $NF}')$(echo $stream1part2)$(echo $stream1part3)$(echo $stream1part4) \
-			--argjson stream2index $(echo $index2) \
-			--arg stream2expected $(echo $stream2part1 | awk '{print $NF}')$(echo $stream2part2)$(echo $stream2part3)$(echo $stream2part4) \
-			--argjson stream3index $(echo $index3) \
-			--arg stream3expected $(echo $stream3part1 | awk '{print $NF}')$(echo $stream3part2)$(echo $stream3part3)$(echo $stream3part4) \
-			--argjson stream4index $(echo $index4) \
-			--arg stream4expected $(echo $stream4part1 | awk '{print $NF}')$(echo $stream4part2)$(echo $stream4part3)$(echo $stream4part4) \
-			--arg xordigest $(echo $xorpart1 | awk '{print $NF}')$(echo $xorpart2)$(echo $xorpart3)$(echo $xorpart4) \
-			'{'$name2':$ARGS.named}'
-	fi
+	args=(
+		--arg key1 "$(echo $key1 | awk '{print $NF}')"
+		$(if [ $1 = 256 ]; then echo "--arg key2 $(echo $key2 | awk '{print $NF}')"; fi)
+		--arg iv "$(echo $iv | awk '{print $NF}')"
+		--argjson stream1index "$(echo $index1)"
+		--arg stream1expected "$(echo $stream1part1 | awk '{print $NF}')$(echo $stream1part2)$(echo $stream1part3)$(echo $stream1part4)"
+		--argjson stream2index "$(echo $index2)"
+		--arg stream2expected "$(echo $stream2part1 | awk '{print $NF}')$(echo $stream2part2)$(echo $stream2part3)$(echo $stream2part4)"
+		--argjson stream3index "$(echo $index3)"
+		--arg stream3expected "$(echo $stream3part1 | awk '{print $NF}')$(echo $stream3part2)$(echo $stream3part3)$(echo $stream3part4)"
+		--argjson stream4index "$(echo $index4)"
+		--arg stream4expected "$(echo $stream4part1 | awk '{print $NF}')$(echo $stream4part2)$(echo $stream4part3)$(echo $stream4part4)"
+		--arg xordigest "$(echo $xorpart1 | awk '{print $NF}')$(echo $xorpart2)$(echo $xorpart3)$(echo $xorpart4)"
+	)
+
+    jq -n "${args[@]}" '{"'$name2'":$ARGS.named}'
 
 done > $tmpfile
 
